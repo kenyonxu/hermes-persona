50081962e2c1910db656add60834311bb5f4713b
Kai.Xu <kenyon1977@gmail.com>
2026-05-16 23:13:16 +0800

    fix: use relative import from .hermes_persona

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ This file lets Hermes discover the register() function from the plugin root.
 The actual implementation lives in the hermes_persona/ package.
 """
 
-from hermes_persona import register
+from .hermes_persona import register
 
 __all__ = ["register"]
